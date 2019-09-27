890181deddbccb93a06f3055a314892bf73fc229
Robert Ing <ring@mparticle.com>
2019-09-27 16:29:28 -0400

    Add appmeasurement to server side, heartbeat cleanup

--- HeartbeatKit/test/boilerplate/test-karma.js
+++ HeartbeatKit/test/boilerplate/test-karma.js
@@ -3,6 +3,6 @@ var util = require('util');
 var defaultArgs = '--single-run --browsers=ChromeHeadless,FirefoxHeadless';
 var args = process.argv.slice(2).join(' ') || defaultArgs;
 
-var testCommand = util.format('browserify test/boilerplate/test-index.js -o test/test-bundle.js && karma start %s test/boilerplate/karma.config.js', args);
+var testCommand = util.format('browserify HeartbeatKit/test/boilerplate/test-index.js -o HeartbeatKit/test/test-bundle.js && karma start %s HeartbeatKit/test/boilerplate/karma.config.js', args);
 console.log('Running command: \n' + testCommand);
 shell.exec(testCommand);
