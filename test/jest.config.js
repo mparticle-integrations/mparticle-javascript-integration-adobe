0e9b1318fb446c0cd8a62294b03362bfd5c23acd
BrandonStalnaker <33703490+BrandonStalnaker@users.noreply.github.com>
2020-05-27 12:06:16 -0400

    Add Ad Position and Revise Placement

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,14 +1,10 @@
 var MODULE = process.env.MODULE;
 
 var files = {
-    client: 'packages/AdobeClient/src/AdobeClientSideKit.js',
-    server: 'packages/AdobeServer/src/AdobeServerSideKit.js'
+    client: '../packages/AdobeClient/src/AdobeClientSideKit.js',
+    server: '../packages/AdobeServer/src/AdobeServerSideKit.js'
 };
 
 module.exports = {
-    setupFiles: [
-        'test/setup/mParticle.js',
-        'test/mockhttprequest.js',
-        files[MODULE]
-    ]
+    setupFiles: ['./setup/mParticle.js', './mockhttprequest.js', files[MODULE]]
 };
